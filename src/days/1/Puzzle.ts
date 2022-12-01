1c543a544930c1500116cb2239f8c5fec7bc38d3
cfbonner <cfbonner@gmail.com>
2022-12-01 17:42:04 +0000

    Clean up day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -2,42 +2,38 @@ import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
   calculateCalories() {
-    type Elf = {
-      items: number[];
-      total: number;
-    };
-
-    let acc: Elf[] = [];
-    const batched = this.input.split('\n\n');
+    const batched: string[] = this.input.split('\n\n');
+    let acc: number[] = [];
 
     batched.forEach((batch) => {
-      const items = batch.split('\n').filter(el => el).map((item) => parseInt(item))
-      acc = [
-        ...acc,
-        {
-          items: items,
-	  total: items.reduce((sum: number, calories: number) => {
-	    return sum + calories;
-	  }, 0)
-	}
-      ];
+      const items = batch
+        .split('\n')
+        .filter((el) => el)
+        .map((item) => parseInt(item));
+      acc.push(
+        items.reduce((sum: number, calories: number) => {
+          return sum + calories;
+        }, 0)
+      );
     });
 
-    return acc.sort((a: Elf, b: Elf) => {
-      return b.total - a.total;
+    return acc.sort((a: number, b: number) => {
+      return b - a;
     });
   }
 
   public solveFirst(): string {
     const sorted = this.calculateCalories();
-    return sorted[0].total.toString();
+    return sorted[0].toString();
   }
 
   public solveSecond(): string {
     const sorted = this.calculateCalories();
-    return [sorted[0].total, sorted[1].total, sorted[2].total].reduce((sum: number, calories: number) => {
-      return sum + calories
-    }, 0).toString()
+    return [sorted[0], sorted[1], sorted[2]]
+      .reduce((sum: number, calories: number) => {
+        return sum + calories;
+      }, 0)
+      .toString();
   }
 
   public getFirstExpectedResult(): string {
