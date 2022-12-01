5a4a9e0f2f0e572511a9423f1928380a990d587b
cfbonner <cfbonner@gmail.com>
2022-12-01 12:02:08 +0000

    Solve day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,17 +1,50 @@
 import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
-  public solveFirst(): string {
-    return 'day 1 solution 1';
+  calculateCalories() {
+    type Elf = {
+      items: number[];
+      total: number;
+    };
+
+    let acc: Elf[] = [];
+    const batched = this.input.split('\n\n');
+
+    batched.forEach((batch) => {
+      const items = batch.split('\n').filter(el => el).map((item) => parseInt(item))
+      acc = [
+        ...acc,
+        {
+          items: items,
+	  total: items.reduce((sum: number, calories: number) => {
+	    return sum + calories;
+	  }, 0)
+	}
+      ];
+    });
+
+    return acc.sort((a: Elf, b: Elf) => {
+      return b.total - a.total;
+    });
   }
+
+  public solveFirst(): string {
+    const sorted = this.calculateCalories();
+    return sorted[0].total.toString();
+  }
+
   public solveSecond(): string {
-    return 'day 1 solution 2';
+    const sorted = this.calculateCalories();
+    return [sorted[0].total, sorted[1].total, sorted[2].total].reduce((sum: number, calories: number) => {
+      return sum + calories
+    }, 0).toString()
   }
 
   public getFirstExpectedResult(): string {
-    return 'day 1 solution 1';
+    return '75501';
   }
+
   public getSecondExpectedResult(): string {
-    return 'day 1 solution 2';
+    return '215594';
   }
 }
